52b48cd6c5901ddc30325ec4369163f04d182d6c
Gabriel FOURNIER <gabfou95@gmail.com>
2019-10-12 22:26:57 +0200

    dsdf

---
+++ Assets/Scripts/EnemyDebile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[RequireComponent(typeof(Spawner))]
+public class EnemyDebile : MonoBehaviour
+{
+    [System.Serializable]
+    public class MeleeUnitRatio
+    {
+        public MeleeUnit meleeUnit;
+        public float ratio;
+
+    }
+    public List<MeleeUnitRatio> listToSpawnWithRatio = null;
+    public float chanceToSpawnEnemybySecond = 1;
+    float totalRatio = 0;
+    Spawner spawner;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        spawner = GetComponent<Spawner>();
+        listToSpawnWithRatio.ForEach(e => totalRatio += e.ratio);
+    }
+
+    MeleeUnit chooseUnit()
+    {
+        float rand = Random.Range (0f, totalRatio);
+        foreach (var unit in listToSpawnWithRatio)
+        {
+            if ((rand -= unit.ratio) < 0)
+                return (unit.meleeUnit);
+        }
+        return (listToSpawnWithRatio.LastOrDefault().meleeUnit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.deltaTime > Random.Range(0f, 1f / chanceToSpawnEnemybySecond))
+            spawner.unityInvoke(chooseUnit());
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public int nblane = 2;
+    public void unityInvoke(MeleeUnit unit)
+    {
+        GameObject.Instantiate(unit);
+        unit.lane = Random.Range(0, nblane) + 1; 
+    }
+
+}
