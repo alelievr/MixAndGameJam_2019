32db8ad9a61880ff106cdbde71257f47f8380e63
Jean ROMAGNANI <jromagna@student.42.fr>
2019-10-12 07:24:07 +0200

    faut push avant gab

--- Assets/Scripts/unit/MeleeUnit.cs
+++ Assets/Scripts/unit/MeleeUnit.cs
@@ -5,51 +5,130 @@ using UnityEngine;
 public class MeleeUnit : MonoBehaviour
 {
 
+    public BoxCollider boxCol;
+    Rigidbody rb;
     public int health = 10;
 
     public float speed = 1f;
+    int direction = 1;
 
     public int lane = 1;
-
+    Vector3 lanepos;
     bool moving = true;
+
+    public GameObject weapon;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = gameObject.GetComponent<Rigidbody>();
+        if (this.gameObject.tag == "friendly")
+            direction = 1;
+        if (this.gameObject.tag == "ennemy")
+        {
+            direction = -1;
+            transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+        }
+        // boxCol.center = new Vector3(direction * boxCol.center.x, boxCol.center.y, boxCol.center.z);
+        if (GameManager.instance.mode == ViewMode.TopDown)
+        {
+            lanepos = new Vector3(0, 5 * lane, 0);
+            transform.position = new Vector3(transform.position.x, lanepos.y, lanepos.z);
+        }
+        if (GameManager.instance.mode == ViewMode.SideScroll)
+        {
+            lanepos = new Vector3(0, 5, 0);
+            transform.position = new Vector3(transform.position.x, lanepos.y, lanepos.z);
+        }
+        GameManager.instance.changeMode.AddListener(ChangeMode);
     }
 
+    public void TakeDamage(int dmg)
+    {
+        health -= dmg;
+        if (health <= 0)
+            DeathIsNow();
+    }
+
+    void DeathIsNow()
+    {
+        Destroy(gameObject);
+    }
+    void ChangeMode()
+    {
+        if (GameManager.instance.mode == ViewMode.TopDown)
+        {
+            lanepos = new Vector3(0, 5 * lane, 0);
+            transform.position = new Vector3(transform.position.x - 0.3f * direction, lanepos.y, lanepos.z);
+
+        }
+        if (GameManager.instance.mode == ViewMode.SideScroll)
+        {
+            lanepos = new Vector3(0, 5, 0);
+            transform.position = new Vector3(transform.position.x - 0.3f * direction, lanepos.y, lanepos.z);
+
+        }
+    }
     // Update is called once per frame
     void Update()
     {
+        if (tapper)
+        {
+            cur -= Time.deltaTime;
+            if (cur < 0)
+            {
+                weapon.SetActive(true);
+                cur = atkspeed;
+            }
+        }
+
+
 
     }
 
+    bool tapper = false;
+    float atkspeed = 2f;
+    float cur = 2f;
 
-
-    void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
-        Debug.Log("collide " + other.tag);
-        if (other.tag == "ennemy")
+        if (other.gameObject.tag != this.gameObject.tag)
         {
-            moving = false;
+            // animator.settrigger("attack");
+            tapper = true;
         }
     }
 
-    private void OnTriggerExit(Collider other)
+
+    void OnTriggerExit(Collider other)
     {
-        Debug.Log("exit " + other.tag);
-        if (other.tag == "ennemy")
+        if (other.gameObject.tag != this.gameObject.tag)
         {
-            moving = true;
+            // animator.settrigger("attack");
+            tapper = false;
         }
     }
+
+
 
     void FixedUpdate()
     {
+        RaycastHit hit;
+
+        Debug.DrawLine(transform.position, new Vector3(transform.position.x + 0.8f * direction, transform.position.y, transform.position.z), Color.green);
+
+        if (Physics.Raycast(transform.position, new Vector3(1 * direction, 0, 0), out hit, 0.8f) == true)
+        {
+            moving = false;
+        }
+        else
+            moving = true;
+        rb.velocity = Vector3.zero;
         if (moving == true)
         {
-            transform.position = new Vector3(transform.position.x + Time.fixedDeltaTime * speed, transform.position.y, transform.position.z);
+
+            rb.velocity = new Vector3(speed * direction, 0, 0);
+            Debug.DrawLine(transform.position, transform.position + new Vector3(5 * direction, 5, 5), Color.red);
         }
     }
 }
---
+++ Assets/Scripts/unit/Weapon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public int damage = 2;
+    void OnTriggerEnter(Collider other)
+    {
+        MeleeUnit hit = other.gameObject.GetComponent<MeleeUnit>();
+        if (other.tag != this.gameObject.tag && hit)
+        {
+            Debug.DrawLine(transform.position, other.transform.position, Color.red, 1f);
+            Debug.DrawRay(transform.position, Vector3.up, Color.green);
+            hit.TakeDamage(damage);
+            gameObject.SetActive(false);
+        }
+    }
+}
