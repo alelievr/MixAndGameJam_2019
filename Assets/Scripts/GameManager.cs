0ed5ae9b99bc0febdec71314d3345b9d6b9f0f30
Jean ROMAGNANI <jromagna@student.42.fr>
2019-10-12 02:58:37 +0200

    gamemanager et scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+    float timeBeforeCall = 2.0f;
+    float timer;
+    public UnityEvent changeEvent = null;
+    public GameManager instance = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance != null){
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        if (changeEvent == null)
+        {
+            changeEvent = new UnityEvent();
+            
+        }
+        changeEvent.AddListener(CallForChange);
+        timer = timeBeforeCall;
+        DontDestroyOnLoad(this);
+    }
+
+    void CallForChange()
+    {
+        Debug.Log("ITS TIME FOR CHANGE!!!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            changeEvent.Invoke();
+            timer = timeBeforeCall;
+        }
+    }
+}
---
+++ Assets/Scripts/unit/MeleeUnit.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeUnit : MonoBehaviour
+{
+
+    public int health = 10;
+
+    public float speed = 1f;
+
+    public int lane = 1;
+
+    bool moving = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("collide " + other.tag);
+        if (other.tag == "ennemy")
+        {
+            moving = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("exit " + other.tag);
+        if (other.tag == "ennemy")
+        {
+            moving = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (moving == true)
+        {
+            transform.position = new Vector3(transform.position.x + Time.fixedDeltaTime * speed, transform.position.y, transform.position.z);
+        }
+    }
+}
