bfe722e2d2687035fdf0066efb557d73a32a572c
alelievr <alelievr@student.42.fr>
2019-10-12 02:25:35 +0200

    Added camera controller

--- Assets/Scripts/SimpleCameraController.cs
+++ Assets/Scripts/SimpleCameraController.cs
@@ -79,14 +79,6 @@ namespace UnityTemplateProjects
         Vector3 GetInputTranslationDirection()
         {
             Vector3 direction = new Vector3();
-            if (Input.GetKey(KeyCode.W))
-            {
-                direction += Vector3.forward;
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                direction += Vector3.back;
-            }
             if (Input.GetKey(KeyCode.A))
             {
                 direction += Vector3.left;
@@ -94,14 +86,6 @@ namespace UnityTemplateProjects
             if (Input.GetKey(KeyCode.D))
             {
                 direction += Vector3.right;
-            }
-            if (Input.GetKey(KeyCode.Q))
-            {
-                direction += Vector3.down;
-            }
-            if (Input.GetKey(KeyCode.E))
-            {
-                direction += Vector3.up;
             }
             return direction;
         }
