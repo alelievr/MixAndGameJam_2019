ce4217763b88b4c014e4514d7e6d8577c0545798
alelievr <alelievr@student.42.fr>
2019-10-13 18:50:48 +0200

    Fixed parallax

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public enum Strategy
+{
+    Easy,
+}
+
+[RequireComponent(typeof(Spawner))]
+public class EnemyAI : MonoBehaviour
+{
+    public int  startGold = 50;
+    int         gold;
+
+    public Strategy strategy;
+    Spawner         spawner;
+
+    public MeleeUnit[]   spawnableEnemies;
+
+    List<MeleeUnit>     enemiesByPrice;
+    bool                isSpawning = false;
+
+    void Start()
+    {
+        gold = startGold;
+        spawner = GetComponent<Spawner>();
+        enemiesByPrice = spawnableEnemies.ToList().OrderBy(m => m.price).ToList();
+    }
+
+    void Update()
+    {
+        switch (strategy)
+        {
+            default:
+            case Strategy.Easy:
+                UpdateEasy();
+                break;
+        }
+    }
+
+    GameObject FindEnemy(UnitType type) => spawnableEnemies.FirstOrDefault(m => m.type == type).gameObject;
+
+    IEnumerator Spawn(MeleeUnit enemy)
+    {
+        gold -= enemy.price;
+        isSpawning = true;
+        spawner.unitInvoke(enemy.gameObject, true, Random.value > 0.5f ? 0 : 1);
+        yield return new WaitForSeconds(1f);
+        isSpawning = false;
+    }
+
+    void UpdateEasy()
+    {
+        if (isSpawning)
+            return;
+
+        // TODO: wait if there is no danger and spawn bigger units
+
+        foreach (var e in enemiesByPrice)
+        {
+            if (gold > e.price * 2)
+                StartCoroutine(Spawn(e));
+        }
+    }
+}
