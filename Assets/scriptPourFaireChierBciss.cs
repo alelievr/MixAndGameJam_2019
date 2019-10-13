a5d225156171fd6c83049a321cbe1c333cae02ea
alelievr <alelievr@student.42.fr>
2019-10-14 00:06:11 +0200

    Derp, asset redirection in dev mode

--- Assets/scriptPourFaireChierBciss.cs
+++ Assets/scriptPourFaireChierBciss.cs
@@ -8,10 +8,29 @@ public class scriptPourFaireChierBciss : MonoBehaviour
     public Image image;
     public Image other;
 
+    Button      button;
+
+    public bool selectedByDefault = false;
+
+    public static int selected = 0;
+
+    void Start()
+    {
+        button = GetComponent< Button >();
+        if (selectedByDefault)
+            selected = GetHashCode();
+    }
+
+    void Update()
+    {
+        image.color = selected == GetHashCode() ? new Color( image.color.r, image.color.r, image.color.r, 1f) : new Color( image.color.r, image.color.r, image.color.r, 0.50f);
+    }
+
     public void difficulty(float diff)
     {
         GameManager.instance.pourcentqgeDegatEnemy = diff;
         image.color = new Color( image.color.r, image.color.r, image.color.r, 1f);
         other.color = new Color( image.color.r, image.color.r, image.color.r, 0.50f);;
+        selected = GetHashCode();
     }
 }
